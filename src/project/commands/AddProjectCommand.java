689c0384e0855b5aa7c333643f133007744f7127
elukyanchikova <lukyanchikowae@gmail.com>
2020-05-03 18:39:32 +0300

    add commands structure

---
+++ src/project/commands/AddProjectCommand.java
@@ -0,0 +1,10 @@
+package project.commands;
+
+import sun.tools.jar.CommandLine;
+
+public class AddProjectCommand implements Command {
+    @Override
+    public void execute(CommandLine commandLine) {
+
+    }
+}
---
+++ src/project/commands/HelpCommand.java
@@ -0,0 +1,10 @@
+package project.commands;
+
+import sun.tools.jar.CommandLine;
+
+public class HelpCommand implements Command {
+    @Override
+    public void execute(CommandLine commandLine) {
+
+    }
+}
